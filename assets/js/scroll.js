5e2142f970e76aed1905fbd9f83d74578a29342e
Anthony D'Alessandro <tony.dalessandro80@gmail.com>
2023-08-15 23:12:37 +0000

    this is good

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,16 +2,17 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs
 
 const swiper = new Swiper('.swiper', {
     loop: true,
+    slidesPerView: 3,
     speed: 600,
     autoplay: {
         delay: 500,
     },
     breakpoints: {
         640: { // sm
-            slidesPerView: 2
+            slidesPerView: 4
         },
         768: { // md
-            slidesPerView: 3
+            slidesPerView: 4
         },
         1024: { // lg
             slidesPerView: 4
